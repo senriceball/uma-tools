827c888d7df7c8b89e8201d4dd3fd4de23839657
pecan <turkchess123@gmail.com>
2023-12-24 20:36:33 -0700

    change default track to hanshin 1400

--- skill-visualizer/app.tsx
+++ skill-visualizer/app.tsx
@@ -21,7 +21,7 @@ import { immediate, noopImmediate } from '../uma-skill-tools/ActivationCondition
 import '../components/Tooltip.css';
 import './app.css';
 
-const DefaultCourseId = 10506;
+const DefaultCourseId = 10902;
 
 const UI_ja = Object.freeze({
 	'title': 'ウマ娘スキル発動位置可視化ツール',
