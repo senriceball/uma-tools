7c653968521d62399bce27f388b7c77883266e9d
pecan <turkchess123@gmail.com>
2025-07-27 04:57:15 -0600

    copy build system from umalator-global (needs to be refactored)

---
+++ umalator/build.mjs
@@ -0,0 +1,47 @@
+import * as esbuild from 'esbuild';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { program } from 'commander';
+
+program
+	.option('--debug');
+
+program.parse();
+const options = program.opts();
+const debug = !!options.debug;
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const mockAssertFn = debug ? 'console.assert' : 'function(){}';
+const mockAssert = {
+	name: 'mockAssert',
+	setup(build) {
+		build.onResolve({filter: /^assert$/}, args => ({
+			path: args.path, namespace: 'mockAssert-ns'
+		}));
+		build.onLoad({filter: /.*/, namespace: 'mockAssert-ns'}, () => ({
+			contents: 'module.exports={strict:'+mockAssertFn+'};',
+			loader: 'js'
+		}));
+	}
+};
+
+const redirectTable = {
+	name: 'redirectTable',
+	setup(build) {
+		build.onResolve({filter: /^@tanstack\//}, args => ({
+			path: path.join(dirname, '..', 'vendor', args.path.slice(10), 'index.ts')
+		}));
+	}
+};
+
+await esbuild.build({
+	entryPoints: [{in: './app.tsx', out: 'bundle'}, './simulator.worker.ts'],
+	bundle: true,
+	minify: !debug,
+	outdir: '.',
+	define: {CC_DEBUG: debug.toString(), CC_GLOBAL: 'false'},
+	external: ['*.ttf'],
+	plugins: [mockAssert, redirectTable]
+});
