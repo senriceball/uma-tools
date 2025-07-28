913b373b37a87e70beb20d7fe5a904d61fe2ad05
pecan <turkchess123@gmail.com>
2025-07-27 23:32:01 -0600

    autocapture fewer events

--- umalator/telemetry.ts
+++ umalator/telemetry.ts
@@ -4,7 +4,14 @@ export function initTelemetry() {
 	if (CC_GLOBAL && !CC_DEBUG) {
 		posthog.init('phc_rmAEubU5JeFpYm6HSMhR8cpe6nbY6eR94jwOpQt0kzt', {
 			api_host: 'https://us.i.posthog.com',
-			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+			person_profiles: 'identified_only',  // or 'always' to create profiles for anonymous users as well
+			loaded: function (posthog) {
+				window.posthog = posthog;
+			},
+			autocapture: {
+				dom_event_allowlist: ['click'],
+				element_allowlist: ['a', 'button', 'select']
+			}
 		});
 	}
 }
